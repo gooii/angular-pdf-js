685504176a159db9b1c04ad0ebda63df8f4dd895
Martin Wood-Mitrovski <martin.wood@gooii.com>
2015-10-01 14:56:00 +0200

    latest pdf.js

--- PDF.js/ui_utils.js
+++ PDF.js/ui_utils.js
@@ -17,7 +17,8 @@
 'use strict';
 
 var CSS_UNITS = 96.0 / 72.0;
-var DEFAULT_SCALE = 'auto';
+var DEFAULT_SCALE_VALUE = 'auto';
+var DEFAULT_SCALE = 1.0;
 var UNKNOWN_SCALE = 0;
 var MAX_AUTO_SCALE = 1.25;
 var SCROLLBAR_PADDING = 40;
@@ -73,6 +74,12 @@ var CustomStyle = (function CustomStyleClosure() {
 
   return CustomStyle;
 })();
+
+var NullCharactersRegExp = /\x00/g;
+
+function removeNullCharacters(str) {
+  return str.replace(NullCharactersRegExp, '');
+}
 
 function getFileName(url) {
   var anchor = url.indexOf('#');
